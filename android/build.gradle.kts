6d6f56dc71c206a61babcc128f785bd188f89efb
City Guest Admin <admin@cityguest.com>
2026-08-20 14:19:45 +0530

    Update file_picker to ^9.0.0 and remove subproject compileSdkVersion override to eliminate Gradle lifecycle error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,22 +19,7 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    val subproject = this
-    fun applyCompileSdk() {
-        if (subproject.hasProperty("android")) {
-            val android = subproject.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
-            android?.compileSdkVersion(36)
-        }
-    }
-    if (subproject.state.executed) {
-        applyCompileSdk()
-    } else {
-        subproject.afterEvaluate {
-            applyCompileSdk()
-        }
-    }
-}
+
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
